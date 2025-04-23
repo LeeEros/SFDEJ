47a354e5e1597a6cb2acc5c2756d82be885d6545
leeeros <leeeros@protonmail.com>
2025-04-23 15:02:43 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import { app } from "@/app";
 const PORT = 3333;
 
 app.listen(PORT, () =>
-  console.log(`Server online em: http://localhost:${PORT}`)
+  console.log(`Servidor online em: http://localhost:${PORT}`)
 );
