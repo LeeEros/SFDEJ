630c6b158d9fe6fa65f0fa74f1ad407c22afdfba
leeeros <leeeros@protonmail.com>
2025-04-26 18:26:08 -0300

    Update diretoria-routes.ts

--- backend/src/modules/diretorias/diretoria-routes.ts
+++ backend/src/modules/diretorias/diretoria-routes.ts
@@ -5,5 +5,9 @@ const diretoriasRoutes = Router();
 const diretoriasController = new DiretoriasController();
 
 diretoriasRoutes.get("/", diretoriasController.findAll);
+diretoriasRoutes.get("/:id", diretoriasController.findById);
+diretoriasRoutes.post("/", diretoriasController.create);
+diretoriasRoutes.put("/:id", diretoriasController.update);
+diretoriasRoutes.delete("/:id", diretoriasController.delete);
 
 export { diretoriasRoutes };
