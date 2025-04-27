795e07d69b295a365bf9997704db5debb07e3d84
leeeros <leeeros@protonmail.com>
2025-04-27 14:50:47 -0300

    Update diretorias-controllers.ts

--- backend/src/modules/diretorias/diretorias-controllers.ts
+++ backend/src/modules/diretorias/diretorias-controllers.ts
@@ -1,4 +1,3 @@
-import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
 import { DiretoriasService } from "./diretoria-service";
 
@@ -6,59 +5,26 @@ const diretoriaService = new DiretoriasService();
 
 export class DiretoriasController {
   async findAll(request: Request, response: Response) {
-    try {
-      const diretorias = await diretoriaService.FindAll();
-      return response.status(200).json(diretorias);
-    } catch (error) {
-      if (error instanceof AppError) {
-        return response
-          .status(error.statusCode)
-          .json({ message: error.message });
-      }
-      return response.status(500).json({ message: "Erro interno do servidor" });
-    }
+    const diretorias = await diretoriaService.FindAll();
+    return response.status(200).json(diretorias);
   }
 
   async findById(request: Request, response: Response) {
     const { id } = request.params;
-    try {
-      const diretoria = await diretoriaService.findById(Number(id));
-      return response.status(200).json(diretoria);
-    } catch (error) {
-      if (error instanceof AppError) {
-        return response
-          .status(error.statusCode)
-          .json({ message: error.message });
-      }
-      return response.status(500).json({ message: "Erro interno do servidor" });
-    }
+
+    const diretoria = await diretoriaService.findById(Number(id));
+    return response.status(200).json(diretoria);
   }
 
   async create(request: Request, response: Response) {
-    try {
-      const diretoria = await diretoriaService.create(request.body);
-      return response.status(201).json(diretoria);
-    } catch (error) {
-      if (error instanceof AppError) {
-        return response
-          .status(error.statusCode)
-          .json({ message: "Erro interno do servidor" + error });
-      }
-    }
+    const diretoria = await diretoriaService.create(request.body);
+    return response.status(201).json(diretoria);
   }
 
   async update(request: Request, response: Response) {
-    try {
-      const { id } = request.params;
-      const diretoria = await diretoriaService.update(Number(id), request.body);
-      return response.status(200).json(diretoria);
-    } catch (error) {
-      if (error instanceof AppError) {
-        return response
-          .status(error.statusCode)
-          .json({ message: "Erro interno do servidor" + error });
-      }
-    }
+    const { id } = request.params;
+    const diretoria = await diretoriaService.update(Number(id), request.body);
+    return response.status(200).json(diretoria);
   }
 
   async delete(request: Request, response: Response) {
