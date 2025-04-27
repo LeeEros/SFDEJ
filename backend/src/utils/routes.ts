5147e6bce8d547a63d56534a9de74ba47039f323
leeeros <leeeros@protonmail.com>
2025-04-27 00:34:05 -0300

    Fix: Routes

--- backend/src/utils/routes.ts
+++ backend/src/utils/routes.ts
@@ -1,11 +1,13 @@
 import { diretoriasRoutes } from "@/modules/diretorias/diretoria-routes";
+import { ejRoutes } from "@/modules/ej/ej-routes";
 import { sessaoRoutes } from "@/modules/sessao/sessao-routes";
 import { usuariosRoutes } from "@/modules/usuarios/usuarios-routes";
 import { Router } from "express";
 
 const routes = Router();
-routes.use("/usuarios", usuariosRoutes);
 routes.use("/sessao", sessaoRoutes);
+routes.use("/usuarios", usuariosRoutes);
+routes.use("ejs", ejRoutes);
 routes.use("/diretorias", diretoriasRoutes);
 
 export { routes };
