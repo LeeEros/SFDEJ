5235c69206d1c0fdd9ca4a0a7f859aa27d6ccf65
leeeros <leeeros@protonmail.com>
2025-04-28 16:29:43 -0300

    Update fb-historico-schema.ts

--- backend/src/modules/feedback/fb-historico/fb-historico-schema.ts
+++ backend/src/modules/feedback/fb-historico/fb-historico-schema.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-export const feedback_historicoSchema = z.object({
+export const feedbackHistoricoSchema = z.object({
   media_geral: z.number(),
   media_categorias: z.number(),
   data_atualizacao: z.date().optional(),
