201a5e1ec4d33231a47db28d8f6854bb0e2aa453
leeeros <leeeros@protonmail.com>
2025-04-28 16:28:19 -0300

    Update fb-historico-schema.ts

--- backend/src/modules/feedback/fb-historico/fb-historico-schema.ts
+++ backend/src/modules/feedback/fb-historico/fb-historico-schema.ts
@@ -4,5 +4,5 @@ export const feedback_historicoSchema = z.object({
   media_geral: z.number(),
   media_categorias: z.number(),
   data_atualizacao: z.date().optional(),
-  fk_feedback: z.number(),
+  fk_feedback: z.number().int(),
 });
