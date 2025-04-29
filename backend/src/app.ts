adb811ee737e188f28f213f1bb889f8f5ef30f75
leeeros <leeeros@protonmail.com>
2025-04-29 16:09:39 -0300

    Feat: Cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import { tratamentoErros } from "./middlewares/tratamento.erros";
 import { routes } from "./utils/routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
