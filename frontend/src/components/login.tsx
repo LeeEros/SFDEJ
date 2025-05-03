c6d2dc03fcc88aba307954ca2bb25880a2e297c3
leeeros <leeeros@protonmail.com>
2025-05-03 15:36:11 -0300

    Update login.tsx

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -44,7 +44,7 @@ export default function Login() {
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Acesse sua conta.
           </h2>
         </div>
 
@@ -92,7 +92,7 @@ export default function Login() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Acessar
               </button>
             </div>
           </form>
