43ef585cfd16bdd0d5e626ec98209eff162ade8c
leeeros <leeeros@protonmail.com>
2025-05-05 11:34:07 -0300

    Feat: login e home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
 import Login from "./components/login";
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export function App() {
-  return <Login />
-}
+export default App;
