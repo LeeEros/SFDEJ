7dbd3f9d819da16099004adb145df4231e37a000
leeeros <leeeros@protonmail.com>
2025-05-05 15:46:56 -0300

    Feat: Valida token

--- backend/src/modules/sessao/sessao-routes.ts
+++ backend/src/modules/sessao/sessao-routes.ts
@@ -5,5 +5,7 @@ const sessaoRoutes = Router();
 const sessaoController = new SessaoController();
 
 sessaoRoutes.post("/", sessaoController.findUsuario);
+// "http://localhost:3000/login/validar-token"
+sessaoRoutes.post("/validar-token", sessaoController.validarToken);
 
 export { sessaoRoutes };
--- frontend/src/components/rotasAutenticadas.tsx
+++ frontend/src/components/rotasAutenticadas.tsx
@@ -1,11 +1,47 @@
 // ProtectedRoute.tsx
-import { JSX } from "react";
+import { JSX, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
-export default function  AutenticarRota({ children }: { children: JSX.Element }) {
+export default function AutenticarRota({ children }: { children: JSX.Element }) {
+  const [isValid, setIsValid] = useState<boolean | null>(null);
   const token = localStorage.getItem("jwtToken");
+  const api = "http://localhost:3333/login/validar-token"
 
-  if (!token) {
+  useEffect(() => {
+    const validarToken = async () => {
+      if (!token) {
+        setIsValid(false);
+        return;
+      }
+
+      try {
+        const response = await fetch(api, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          setIsValid(true);
+        } else {
+          setIsValid(false);
+        }
+      } catch (error) {
+        console.error("Erro ao validar token:", error);
+        setIsValid(false);
+      }
+    };
+
+    validarToken();
+  }, [token]);
+
+  if (isValid === null) {
+    return <div>Carregando...</div>;
+  }
+
+  if (!isValid) {
     return <Navigate to="/login" />;
   }
 
