834c1fae127f36baa80b7b9a069c3e72250e3fcd
leeeros <leeeros@protonmail.com>
2025-05-14 09:59:48 -0300

    Fix: Routers

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,49 +1,111 @@
-import type React from "react"
-import { Button } from "../components/Button"
-import { Input } from "../components/Input"
-import { useState } from "react"
-import clsx from "clsx"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export function Login() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [carregando, setCarregando] = useState(false)
+    const api = "http://localhost:3333/"
+    const rota = "login"
 
-    function onSubmit(e: React.FormEvent) {
-        e.preventDefault()
-        console.log(email, senha);
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(api + rota, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, senha }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const { token } = data;
+
+        localStorage.setItem("jwtToken", token);
+        navigate("/home");
+      } else {
+        setError("Erro ao fazer login. Verifique suas credenciais.");
+      }
+    } catch (error) {
+      setError("Erro na requisição. Tente novamente mais tarde.");
     }
+  };
 
-    return (<form onSubmit={onSubmit} className="w-full flex flex-col gap-6">
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="tailwind logo"
+            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Acesse sua conta.
+          </h2>
+        </div>
 
-        <Input
-            required
-            legenda="Email"
-            type="email"
-            placeholder="email@email.com"
-            onChange={(e) => setEmail(e.target.value)}
-        />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleLogin} className="space-y-6">
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
 
-        <Input
-            required
-            legenda="Senha"
-            type="password"
-            placeholder="password"
-            onChange={(e) => setSenha(e.target.value)}
-        />
+            <div>
+              <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                Senha
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  value={senha}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
 
-        <Button type="submit" carregando={carregando}>Entrar</Button>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Acessar
+              </button>
+            </div>
+          </form>
 
-        <a href="/criar-conta"
-            className={
-                clsx(
-                    "text-sm font-semibold text-gray-100 mt-10 mb-4",
-                    "text-center hover:text-indigo-300 transition ease-linear"
-                )
-            }
-        >
-            Criar conta
-        </a>
-    </form>)
-}
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Não registrado?{" "}
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Faça parte do MEJ!
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
