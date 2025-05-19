13c19bedda2bace65d5f7d2a1e14549047ce01bf
leeeros <leeeros@protonmail.com>
2025-05-19 19:34:52 -0300

    Update AppRoutes.tsx

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AutenticarRota from "../components/rotasAutenticadas";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
 import { NotFound } from "../pages/NotFound";
+import Home from "../pages/home";
+import Login from "../pages/login";
 
 
 const App = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/home" index element={ <AutenticarRota><Home /></AutenticarRota>} />
         <Route path="/login" element={<Login />} />
 
         <Route path="*" element = {<NotFound/> }></Route>
       </Routes>
-    </Router>
   );
 };
 
