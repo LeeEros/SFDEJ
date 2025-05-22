c1051e351d12fd172c5d7d9c48eb987822047d07
leeeros <leeeros@protonmail.com>
2025-05-22 16:07:06 -0300

    Create AuthRoutes.tsx

---
+++ frontend/src/routes/AuthRoutes.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router";
+import { AuthLayout } from "../components/AuthLayout";
+
+import { Login } from "../pages/Login";
+import { CriarConta } from "../pages/CriarConta";
+
+
+export function AuthRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<AuthLayout />}>
+                <Route path="/" element={< Login />} />
+                <Route path="/criar-conta" element={< CriarConta />} />
+            </Route>
+        </Routes>
+    )
+}
