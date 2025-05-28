e7b0cc553ea3f71d0862ff2ffcc858b982c0254e
leeeros <leeeros@protonmail.com>
2025-05-28 14:42:15 -0300

    Create Footer.tsx

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="w-screen bg-blue-800 text-white py-6">
+            <div className="container mx-auto text-center">
+                <p>&copy; 2025 SFDEJ. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
+}
