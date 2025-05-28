797ca2da67ce261eb60ad88fdc8ee15d48040b7d
leeeros <leeeros@protonmail.com>
2025-05-28 17:08:12 -0300

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,13 +3,13 @@ import logoutSvg from "../assets/logout.svg"
 
 export function Header() {
     return (
-        <header>
-            <img src={logoSvg} alt="logo" />
+        <header className="w-full flex justify-between">
+            <img src={logoSvg} alt="logo" className="my-8" />
 
-            <div>
-                <span>Olá usuário!</span>
+            <div className="flex items-center gap-3">
+                <span className="text-sm font-semibold text-gray-200">Olá usuário!</span>
 
-                <img src={logoutSvg} alt="ícone de logout/sair" />
+                <img src={logoutSvg} alt="ícone de logout/sair" className="my-8 cursor-pointer hover:opacity-70 transition ease-linear" />
 
             </div>
         </header>
