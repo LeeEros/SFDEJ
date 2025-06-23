b83a1218fdb8d2b074df162826bd0290614c4f4b
leeeros <leeeros@protonmail.com>
2025-06-23 15:10:03 -0300

    Create AppFooter.tsx

---
+++ frontend/src/layout/AppFooter.tsx
@@ -0,0 +1,12 @@
+const AppFooter = () => (
+    <footer className="w-full bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 py-4 px-6 text-center text-sm text-gray-600 dark:text-gray-400">
+        <div>
+            <strong>2025 SFDEJ.</strong> Todos os direitos reservados.
+        </div>
+        <div>
+            Desenvolvido por Lee Eros Galaad.
+        </div>
+    </footer>
+);
+
+export default AppFooter;
