2b99d26c011afe31cc98864009f6774bbc7be503
leeeros <leeeros@protonmail.com>
2025-06-25 22:58:10 -0300

    Update NotFound.tsx

--- frontend/src/pages/OtherPage/NotFound.tsx
+++ frontend/src/pages/OtherPage/NotFound.tsx
@@ -1,6 +1,7 @@
 import GridShape from "../../components/common/GridShape";
 import { Link } from "react-router";
 import PageMeta from "../../components/common/PageMeta";
+import AppFooter from "../../layout/AppFooter";
 
 export default function NotFound() {
   return (
@@ -24,20 +25,20 @@ export default function NotFound() {
           />
 
           <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-            We can’t seem to find the page you are looking for!
+           Não foi possível encontrar a página que você está procurando.
           </p>
 
           <Link
             to="/"
             className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
           >
-            Back to Home Page
+            Voltar para o início.
           </Link>
         </div>
-        {/* <!-- Footer --> */}
-        <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-          &copy; {new Date().getFullYear()} - TailAdmin
-        </p>
+        {/* <!-- Footer   use o footer já pronto --> */}
+
+        <AppFooter />
+
       </div>
     </>
   );
