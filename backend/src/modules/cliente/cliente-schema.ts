7d6c260f5e7c7044f10427fef8ab17f989fb6446
leeeros <leeeros@protonmail.com>
2025-06-25 21:02:44 -0300

    Update cliente-schema.ts

--- backend/src/modules/cliente/cliente-schema.ts
+++ backend/src/modules/cliente/cliente-schema.ts
@@ -4,7 +4,10 @@ export const clienteSchema = z.object({
   nome: z.string().min(5, "Nome deve ter pelo menos 5 letras"),
   CNPJ: z.string().length(14, "CNPJ deve ter 14 caracteres").optional(),
   CPF: z.string().length(14, "CNPJ deve ter 14 caracteres").optional(),
-  email: z.string().email("Email inválido"),
-  telefone: z.string().max(15, "Telfone deve ter no máximo 15 caracteres"),
+  email: z.string().email("Email inválido").optional(),
+  telefone: z
+    .string()
+    .max(15, "Telfone deve ter no máximo 15 caracteres")
+    .optional(),
   fk_endereco: number().optional(),
 });
