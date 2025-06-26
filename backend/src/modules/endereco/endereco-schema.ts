ebeb3a37ffe781b4e7d48e9b198848a90395d681
leeeros <leeeros@protonmail.com>
2025-06-26 09:28:57 -0300

    Fix: S/N endereco

--- backend/src/modules/endereco/endereco-schema.ts
+++ backend/src/modules/endereco/endereco-schema.ts
@@ -5,5 +5,5 @@ export const enderecoSchema = z.object({
   cidade: z.string().max(255),
   estado: z.string().max(255).toUpperCase(),
   endereco: z.string().max(255),
-  numero: z.number().int(),
+  numero: z.number().int().optional(),
 });
