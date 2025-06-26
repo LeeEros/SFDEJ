5e62416ed05363166ab8be5045ccb3575e324eb9
leeeros <leeeros@protonmail.com>
2025-06-26 08:18:32 -0300

    Update SignUp.tsx

--- frontend/src/pages/AuthPages/SignUp.tsx
+++ frontend/src/pages/AuthPages/SignUp.tsx
@@ -6,8 +6,8 @@ export default function SignUp() {
   return (
     <>
       <PageMeta
-        title="React.js SignUp Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js SignUp Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="SFDEJ - Sign Up"
+        description="Sistema de Feedback de Empresários Juniores - Sign Up Page"
       />
       <AuthLayout>
         <SignUpForm />
