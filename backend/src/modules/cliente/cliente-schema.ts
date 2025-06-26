952812bd25c8d0b8526cbd23e663a2131d62eae7
leeeros <leeeros@protonmail.com>
2025-06-26 08:34:52 -0300

    Fix: cpf quantidade

--- backend/src/modules/cliente/cliente-schema.ts
+++ backend/src/modules/cliente/cliente-schema.ts
@@ -3,7 +3,7 @@ import z, { number } from "zod";
 export const clienteSchema = z.object({
   nome: z.string().min(5, "Nome deve ter pelo menos 5 letras"),
   CNPJ: z.string().length(14, "CNPJ deve ter 14 caracteres").optional(),
-  CPF: z.string().length(14, "CNPJ deve ter 14 caracteres").optional(),
+  CPF: z.string().length(11, "CPF deve ter 11 caracteres").optional(),
   email: z.string().email("Email inválido").optional(),
   telefone: z
     .string()
