fe6416249bc5d3674d4b1858a557574258bd1e08
leeeros <leeeros@protonmail.com>
2025-09-15 07:58:09 -0300

    Feat: App Helmet e Rate limit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
 import { tratamentoErros } from "./middlewares/tratamento.erros";
 import { routes } from "./utils/routes";
 import { createAdmin } from "./utils/create-admin";
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message:
+    "Muitas requisições enviadas deste IP, por favor, tente novamente após 15 minutos.",
+});
+
+app.use(helmet());
+app.use(limiter);
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
